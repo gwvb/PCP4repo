cf1f0187d75d4f11ccfdafc4a635bb680bd2090a
Gabriel Wagner vom Berg <42769425+gabelstein@users.noreply.github.com>
2026-05-18 10:06:42 +0200

    first commit

---
+++ transformers_basic.py
@@ -0,0 +1,39 @@
+from scipy.stats import zscore
+import numpy as np
+from sklearn.base import TransformerMixin, BaseEstimator
+
+
+class StandardScaler(BaseEstimator, TransformerMixin):
+    """Scale the data to given mean and std.
+
+    Parameters
+    ----------
+    mean : str
+        Rereferencing method.
+    standard_deviation : int
+        std
+    """
+
+    def __init__(self, mean=None, std=None, axis=0):
+        self.mean = mean
+        self.std = std
+        self.axis = axis
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        return (X - X.mean(keepdims=True)) / (X.std(keepdims=True)
+
+
+class Variance(TransformerMixin, BaseEstimator):
+    """Returns the variance of the data."""
+
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        return np.var(X, axis=0)
